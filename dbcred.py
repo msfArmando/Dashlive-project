2d045d51382ab041ce72dbbea15e07485a5d28e0
msfArmando <armandomonsaof@gmail.com>
2025-06-12 12:11:15 -0300

    Deleting dbcred

--- dbcred.py
+++
@@ -1,6 +0,0 @@
-class DbConnect():
-    driver = "{ODBC Driver 17 for SQL Server};"
-    server = "181.41.170.202,38000;"
-    database = "C8YL0N_134015_RM_PD;"
-    UID = "CLT134015MSTR;"
-    PWD = "htrxk87153SVPDR@?;"
