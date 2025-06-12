64c5e8b6020d5f945b2257737988f40f9e6967b4
msfArmando <armandomonsaof@gmail.com>
2025-06-12 12:08:38 -0300

    implementação inicial do dataset conectado ao banco de dados com consulta temporária para teste

--- dashboards.py
+++ dashboards.py
@@ -1,3 +1,25 @@
+from dbcred import DbConnect as db
+from sqlalchemy import create_engine
+import urllib
+import pandas as pd
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 
+params = urllib.parse.quote_plus(
+    f"DRIVER={db.driver};"
+    f"SERVER={db.server}"
+    f"DATABASE={db.database}"
+    f"UID={db.UID}"
+    f"PWD={db.PWD}"
+)
+#Cria um dicionário usando o método quote_plus que decodifica caracteres especiais para que sejam aceitos em urls
+
+engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
+
+query = """
+SELECT * FROM HATENDIMENTOBASE
+"""
+#Cria uma string com a consulta específica para o banco de dados.
+
+dataset = pd.read_sql(query, engine) #Realiza uma leitura do banco de dados usando esta consulta e retorna os valores como um dataset
---
+++ dbcred.py
@@ -0,0 +1,6 @@
+class DbConnect():
+    driver = "{ODBC Driver 17 for SQL Server};"
+    server = "181.41.170.202,38000;"
+    database = "C8YL0N_134015_RM_PD;"
+    UID = "CLT134015MSTR;"
+    PWD = "htrxk87153SVPDR@?;"
